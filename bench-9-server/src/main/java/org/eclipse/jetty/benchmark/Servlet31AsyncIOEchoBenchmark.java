f86450a89439bf06222afbef78bdf36765acfa88
Greg Wilkins <gregw@intalio.com>
2013-08-15 14:42:18 +1000

    alternative async style

--- bench-9-server/src/main/java/org/eclipse/jetty/benchmark/Servlet31AsyncIOEchoBenchmark.java
+++ bench-9-server/src/main/java/org/eclipse/jetty/benchmark/Servlet31AsyncIOEchoBenchmark.java
@@ -3,6 +3,8 @@ package org.eclipse.jetty.benchmark;
 import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import javax.servlet.AsyncContext;
 import javax.servlet.ReadListener;
 import javax.servlet.ServletException;
@@ -34,6 +36,7 @@ public class Servlet31AsyncIOEchoBenchmark
     private ServerConnector connector;
     private HttpClient client;
     private String asyncPath = "/async";
+    private String asyncPath2 = "/async2";
     private String syncPath = "/sync";
 
     public static void main(String[] args) throws Exception
@@ -57,6 +60,7 @@ public class Servlet31AsyncIOEchoBenchmark
         server.addConnector(connector);
         ServletContextHandler context = new ServletContextHandler(server, "/", false, false);
         context.addServlet(AsyncServlet.class, asyncPath);
+        context.addServlet(AsyncServlet2.class, asyncPath2);
         context.addServlet(SyncServlet.class, syncPath);
         server.start();
 
@@ -76,12 +80,13 @@ public class Servlet31AsyncIOEchoBenchmark
     {
         int runs = 2;
         int iterations = 5_000;
-        int[] lengths = new int[]{1024, 4*1024, 16*1024, 64*1024, 256*1024, 1024*1024, 4*1024*1024};
+        int[] lengths = new int[]{1024, 1024, 1024, 1024, 4*1024, 16*1024, 64*1024, 256*1024, 1024*1024, 4*1024*1024};
         for (int i = 0; i < runs; ++i)
         {
             for (int l = 0; l < lengths.length; ++l)
             {
                 run(iterations, asyncPath, lengths[l]);
+                run(iterations, asyncPath2, lengths[l]);
                 run(iterations, syncPath, lengths[l]);
             }
         }
@@ -196,6 +201,88 @@ public class Servlet31AsyncIOEchoBenchmark
         }
     }
 
+    public static class AsyncServlet2 extends HttpServlet
+    {
+        @Override
+        protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+        {
+            AsyncContext asyncContext = request.startAsync(request, response);
+            asyncContext.setTimeout(0);
+            Echoer echoer = new Echoer(asyncContext);
+            request.getInputStream().setReadListener(echoer);
+            response.getOutputStream().setWriteListener(echoer);
+        }
+
+        private class Echoer implements ReadListener, WriteListener
+        {
+            private final byte[] buffer = new byte[BUFFER_SIZE];
+            private final AsyncContext asyncContext;
+            private final ServletInputStream input;
+            private final ServletOutputStream output;
+            private final AtomicBoolean complete=new AtomicBoolean(false);
+            
+
+            private Echoer(AsyncContext asyncContext) throws IOException
+            {
+                this.asyncContext = asyncContext;
+                this.input = asyncContext.getRequest().getInputStream();
+                this.output = asyncContext.getResponse().getOutputStream();
+            }
+            
+            @Override
+            public void onDataAvailable() throws IOException
+            {
+                // input is available, but only read if we can write
+                if (output.isReady())
+                    onWritePossible();
+            }
+
+            @Override
+            public void onAllDataRead() throws IOException
+            {
+                // All data is consume, so all writes are complete then complete. 
+                if (output.isReady() && complete.compareAndSet(false,true))
+                    asyncContext.complete();
+            }
+
+            @Override
+            public void onWritePossible() throws IOException
+            {
+                // This method is called:
+                //   1) after first registering a WriteListener (ready for first write)
+                //   2) after first registering a ReadListener iff write is ready
+                //   3) when a previous write completes after an output.isReady() returns false
+                //   4) from an input callback if out.isReady()
+                
+                // If there is no more input to consume
+                if (input.isFinished()) 
+                {
+                    if (complete.compareAndSet(false,true))
+                        asyncContext.complete();
+                    return;
+                }
+                
+                // We know we can write, so loop only if we can read as well
+                while (input.isReady())
+                {
+                    int read = input.read(buffer);
+                    output.write(buffer, 0, read);
+                    
+                    // only continue if we can still write
+                    if (!output.isReady())
+                        break;
+                }
+                
+            }
+
+            @Override
+            public void onError(Throwable failure)
+            {
+                failure.printStackTrace();
+            }
+        }
+    }
+    
     public static class SyncServlet extends HttpServlet
     {
         @Override
